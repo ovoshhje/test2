a2fc994c390ec557e8f4ec6364ca2c7069383fec
ovoshhje <73668473+ovoshhje@users.noreply.github.com>
2020-10-29 23:19:11 +0300

    Add files via upload

---
+++ AndroidApp/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Net.Http;
+using System.Threading;
+
+namespace AndroidApp
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstace)
+        {
+            base.OnCreate(savedInstace);
+            var httpclient = new HttpClient();
+            using (CancellationTokenSource tokenSource = new CancellationTokenSource(2_000))
+            {
+                try
+                {
+                    httpclient.GetAsync("https://google.com:4552", tokenSource.Token).GetAwaiter().GetResult(); //Отсутствие ответа от сервера не является ошибкой задания
+                }
+                catch
+                {
+                }
+                finally
+                {
+                }
+            };
+            SetContentView(Resource.Layout.activity_main);
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpclient = new HttpClient();
+            using CancellationTokenSource tokenSource = new CancellationTokenSource(2_000);
+            try
+            {
+                httpclient.GetAsync("https://google.com:4552", tokenSource.Token).GetAwaiter().GetResult(); //Отсутствие ответа от сервера не является ошибкой задания
+            }
+            catch
+            {
+            }
+            finally
+            {
+            }
+        }
+    }
+}
